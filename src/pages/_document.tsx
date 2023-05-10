8795dbbfc91e7827f65d6dfa86eccbcace4583c5
Jeffrey <jbugahod@gmail.com>
2023-05-10 13:45:57 +0800

    Update error in typescript error: cannont find name dataLayer

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,14 +6,21 @@ export default class Document extends NextDocument {
     return (
       <Html>
         <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-19E3SBK9LT"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-19E3SBK9LT');
-        </script>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-19E3SBK9LT"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-19E3SBK9LT');
+              `,
+            }}
+          />
         </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
